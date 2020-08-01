44c2ef25f52dafe12c16037b8de3320c60b8cc31
Robin Frousheger <robin@home.froosh.net>
2020-08-01 17:47:13 +1000

    Initial simplistic framework

---
+++ checkPassword/index.js
@@ -0,0 +1,22 @@
+// Start Azure Application Insights as early as possible
+let appInsights = require("applicationinsights");
+appInsights.setup()
+    .setSendLiveMetrics(true)
+    .start();
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
