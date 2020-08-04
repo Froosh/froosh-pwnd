88122f03031f97ac4ccb60752d7cf13a097cbde3
Robin Frousheger <robin@home.froosh.net>
2020-08-04 17:53:44 +1000

    Start simple parameter handling and logging

--- checkPassword/index.js
+++ checkPassword/index.js
@@ -4,19 +4,26 @@ appInsights.setup()
     .setSendLiveMetrics(true)
     .start();
 
+let https = require("https");
+https.globalAgent.keepAlive = true;
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.body && req.body.password) {
+        let passwordToCheck = req.body.password;
+
+        context.log.verbose("Password to check:", passwordToCheck);
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + passwordToCheck
         };
-    }
-    else {
+    } else {
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            // B2C ValidationProfile requires specific result codes
+            status: 409,
+            body: "Required parameters are missing."
         };
     }
 };
