3e810147e32fd2a021c73e645ea3b1b29468f7ee
Robin Frousheger <robin@home.froosh.net>
2023-02-24 21:10:38 +1100

    A swag of probably unnecessary tweaks as a tinker and mess around

--- checkPassword/index.js
+++ checkPassword/index.js
@@ -1,38 +1,37 @@
 // Always
-"use strict";
+'use strict'
+
+import axios from 'axios'
+
+import { createHash } from 'node:crypto'
+
+import https from 'node:https'
 
 // Start Azure Application Insights before anything else
-import applicationinsights from "applicationinsights";
-applicationinsights.setup().setSendLiveMetrics(true);
-applicationinsights.defaultClient.setAutoPopulateAzureProperties(true);
-applicationinsights.start();
-
-import axios from "axios";
-
-import { createHash } from "node:crypto";
-
-import https from "node:https";
+import applicationinsights from 'applicationinsights'
+applicationinsights.setup()
+applicationinsights.start()
 
 // Have-I-Been-Pwned config details
-const hibpBaseURL = "https://api.pwnedpasswords.com/range/";
+const hibpBaseURL = 'https://api.pwnedpasswords.com/range/'
 const hibpOptions = {
   decompress: true,
   headers: {
-    "Accept-Encoding": "gzip, compress, deflate",
-    "Add-Padding": true,
+    // 'Accept-Encoding': 'gzip, compress, deflate',
+    'Add-Padding': true
   },
-  httpsAgent: new https.Agent({keepAlive: true}),
-  timeout: 1500,
-};
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  timeout: 500
+}
 
 const httpTrigger = async function (context, req) {
   const functionResult = {
     status: 200,
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json; charset=utf-8'
     },
     body: {
-      version: "0.2.4",
+      version: '0.2.5',
       passwordOk: false,
       status: 200,
       userMessage: null,
@@ -41,117 +40,100 @@ const httpTrigger = async function (context, req) {
       code: null,
       developerMessage: null,
       moreInfo: null,
-      requestId: context.invocationId,
-    },
-  };
+      requestId: context.invocationId
+    }
+  }
 
   if (req.body && req.body.password) {
     // Generate SHA1 hash of the password and split it into prefix/suffix
-    const sha1 = createHash("sha1");
-    sha1.update(req.body.password);
-    const sha1Digest = sha1.digest("hex").toUpperCase();
-    const sha1Prefix = sha1Digest.slice(0, 5);
-    const sha1Suffix = sha1Digest.slice(5);
+    const sha1 = createHash('sha1')
+    sha1.update(req.body.password)
+    const sha1Digest = sha1.digest('hex').toUpperCase()
+    const sha1Prefix = sha1Digest.slice(0, 5)
+    const sha1Suffix = sha1Digest.slice(5)
 
-    const hibpURL = encodeURI(`${hibpBaseURL}${sha1Prefix}`);
+    const hibpURL = encodeURI(`${hibpBaseURL}${sha1Prefix}`)
 
-    context.log.info("Checking URL:", hibpURL);
+    context.log.info('Checking URL:', hibpURL)
 
-    hibpOptions.headers["User-Agent"] = req.headers["user-agent"];
+    hibpOptions.headers['User-Agent'] = req.headers['user-agent']
 
-    await axios
-      .get(hibpURL, hibpOptions)
-      .then(function (response) {
-        const dataArray = response.data.split("\r\n");
-        context.log.info(`Received ${dataArray.length} result lines.`);
+    try {
+      await axios
+        .get(hibpURL, hibpOptions)
+        .then(function (response) {
+          const dataArray = response.data.split('\r\n')
+          context.log.info(`Received ${dataArray.length} result lines.`)
 
-        // Chop the received data lines into suffix and count values
-        // Then select the entry which matches the password SHA1 suffix (if any)
-        const filteredArray = dataArray
-          .map(function (line) {
-            const parts = line.split(":");
-            return {
-              suffix: parts[0],
-              count: parts[1],
-            };
-          })
-          .filter(function (entry) {
-            return entry.suffix === sha1Suffix;
-          });
+          // Chop the received data lines into suffix and count values
+          // Then select the entry which matches the password SHA1 suffix (if any)
+          const filteredArray = dataArray
+            .map(function (line) {
+              const parts = line.split(':')
+              return {
+                suffix: parts[0],
+                count: parts[1]
+              }
+            })
+            .filter(function (entry) {
+              return entry.suffix === sha1Suffix
+            })
 
-        // If an entry is found use the result, otherwise 0
-        const sha1DigestCount =
-          filteredArray.length === 1 ? filteredArray[0].count : 0;
-        context.log.info(`Password Found ${sha1DigestCount} times.`);
+          // If an entry is found use the result, otherwise 0
+          const sha1DigestCount =
+            filteredArray.length === 1 ? filteredArray[0].count : 0
+          context.log.info(`Password found ${sha1DigestCount} times.`)
 
-        // Update the response for success/fail
-        if (sha1DigestCount === 0) {
-          functionResult.body.passwordOk = true;
-        } else {
-          functionResult.status = 409;
-          functionResult.body.status = 409;
-          functionResult.body.userMessage = `Your password has been exposed ${sha1DigestCount} times, choose another.`;
-        }
-      })
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response);
+          // Update the response for success/fail
+          if (sha1DigestCount === 0) {
+            functionResult.body.passwordOk = true
+          } else {
+            functionResult.status = 409
+            functionResult.body.status = 409
 
-          functionResult.status = error.response.status;
-          functionResult.body.status = error.response.status;
+            functionResult.body.userMessage = `Your password has been exposed ${sha1DigestCount} times, choose another.`
+          }
+        })
+        .catch(function (error) {
+          functionResult.status = 409
+          functionResult.body.status = 409
 
-          functionResult.body.code = error.response.status;
-          functionResult.body.developerMessage = error.response.data;
-          functionResult.body.moreInfo = error.response.headers;
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
+          functionResult.body.userMessage =
+            'Error checking password against haveibeenpwned.'
 
-          functionResult.body.developerMessage = error.request;
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            context.log.error(error.response)
 
-          functionResult.body.developerMessage = error.message;
-        }
+            functionResult.status = error.response.status
+            functionResult.body.status = error.response.status
 
-        functionResult.body.userMessage =
-          "Error checking password against haveibeenpwned.";
-      });
+            functionResult.body.code = error.response.status
+            functionResult.body.developerMessage = error.response.data
+            functionResult.body.moreInfo = error.response.headers
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            context.log.error(error.message)
+            functionResult.body.developerMessage = error.message
+          }
+        })
+    } catch (error) {
+      functionResult.status = 409
+      functionResult.body.status = 409
+
+      functionResult.body.userMessage =
+        'Error checking password against haveibeenpwned.'
+
+      context.log.error(JSON.stringify(error))
+      functionResult.body.developerMessage = JSON.stringify(error)
+    }
   } else {
-    functionResult.body.userMessage = "No password provided.";
+    functionResult.body.userMessage = 'No password provided.'
   }
 
-  context.log.verbose("Returning Result:", functionResult);
-  context.res = functionResult;
-};
-
-// As per https://github.com/microsoft/ApplicationInsights-node.js#azure-functions
-// To link request and dependency calls correctly
-export default async function (context, req) {
-  // Start an AppInsights Correlation Context using the provided Function context
-  const correlationContext = applicationinsights.startOperation(context, req);
-
-  // Wrap the Function runtime with correlationContext
-  return applicationinsights.wrapWithCorrelationContext(async () => {
-    const startTime = Date.now(); // Start trackRequest timer
-
-    // Run the Function
-    await httpTrigger(context, req);
-
-    // Track Request on completion
-    applicationinsights.defaultClient.trackRequest({
-      name: `${context.req.method} ${context.req.url}`,
-      resultCode: context.res.status,
-      success: true,
-      url: req.url,
-      duration: Date.now() - startTime,
-      id: correlationContext.operation.parentId,
-    });
-    applicationinsights.defaultClient.flush();
-  }, correlationContext)();
+  context.log.verbose('Returning Result:', functionResult)
+  context.res = functionResult
 }
+
+export default httpTrigger
